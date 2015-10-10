28a0f03006538bc241b07d8890554a7dec088765
ordon <ordon1313@gmail.com>
2015-10-10 13:55:52 +0200

    hide drawer

--- mobile/src/main/java/com/mobilization/HomeActivity.java
+++ mobile/src/main/java/com/mobilization/HomeActivity.java
@@ -2,10 +2,16 @@ package com.mobilization;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.MenuItem;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +20,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         getSupportFragmentManager().beginTransaction().replace(R.id.home_fragment_container, new HomeFragment()).commit();
         NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
         navigationView.setNavigationItemSelectedListener(this);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 
     @Override
@@ -21,10 +28,12 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         int itemId = menuItem.getItemId();
         if (itemId == R.id.navigation_item_1) {
             getSupportFragmentManager().beginTransaction().replace(R.id.home_fragment_container, new HomeFragment()).commit();
+            drawerLayout.closeDrawer(GravityCompat.START);
             return true;
         }
         if (itemId == R.id.navigation_item_2) {
             getSupportFragmentManager().beginTransaction().replace(R.id.home_fragment_container, new SecondFragment()).commit();
+            drawerLayout.closeDrawer(GravityCompat.START);
             return true;
         }
         return false;
