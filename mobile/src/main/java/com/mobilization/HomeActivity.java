a00941fa8b5116840107d2e037b754e930630b4d
ordon <ordon1313@gmail.com>
2015-10-10 13:49:37 +0200

    change fragments on click

--- mobile/src/main/java/com/mobilization/HomeActivity.java
+++ mobile/src/main/java/com/mobilization/HomeActivity.java
@@ -1,15 +1,32 @@
 package com.mobilization;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
         getSupportFragmentManager().beginTransaction().replace(R.id.home_fragment_container, new HomeFragment()).commit();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+        int itemId = menuItem.getItemId();
+        if (itemId == R.id.navigation_item_1) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.home_fragment_container, new HomeFragment()).commit();
+            return true;
+        }
+        if (itemId == R.id.navigation_item_2) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.home_fragment_container, new SecondFragment()).commit();
+            return true;
+        }
+        return false;
     }
 }
---
+++ mobile/src/main/java/com/mobilization/SecondFragment.java
@@ -0,0 +1,16 @@
+package com.mobilization;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public final class SecondFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.second_fragment, container, false);
+    }
+}
