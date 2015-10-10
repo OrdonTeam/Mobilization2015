148db64edbddee5f6a4f50da642cc84dc802b8d4
ordon <ordon1313@gmail.com>
2015-10-10 14:01:55 +0200

    snackbar

--- mobile/src/main/java/com/mobilization/HomeFragment.java
+++ mobile/src/main/java/com/mobilization/HomeFragment.java
@@ -2,6 +2,7 @@ package com.mobilization;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,5 +13,24 @@ public final class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.home_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final View coordiatorLayout = view.findViewById(R.id.coordinator_layout);
+        view.findViewById(R.id.show_toast).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Snackbar snackbar = Snackbar.make(coordiatorLayout, "This is a snackbar not toast", Snackbar.LENGTH_INDEFINITE);
+                snackbar.setAction("DELETE", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        snackbar.dismiss();
+                    }
+                });
+                snackbar.show();
+            }
+        });
     }
 }
