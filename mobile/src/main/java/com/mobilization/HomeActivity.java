884ac29c4dc227a6766033a35198464ff426d860
ordon <ordon1313@gmail.com>
2015-10-10 13:41:36 +0200

    added home fragment

--- mobile/src/main/java/com/mobilization/HomeActivity.java
+++ mobile/src/main/java/com/mobilization/HomeActivity.java
@@ -2,6 +2,7 @@ package com.mobilization;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_activity);
+        getSupportFragmentManager().beginTransaction().replace(R.id.home_fragment_container, new HomeFragment()).commit();
     }
 }
