bd155e2d0b64b7964497b17153894ea2b1cf40fa
ordon <ordon1313@gmail.com>
2015-10-11 18:41:18 +0200

    Title

--- mobile/src/main/java/com/mobilization/second/SecondFragment.java
+++ mobile/src/main/java/com/mobilization/second/SecondFragment.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,6 +24,10 @@ public final class SecondFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         ViewPager pager = (ViewPager) view.findViewById(R.id.view_pager);
         pager.setAdapter(new TriplePageAdapter(getFragmentManager()));
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Just a nice title");
 
         TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tab_layout);
         tabLayout.setupWithViewPager(pager);
