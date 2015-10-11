35c97f7a2cf6efbab78374cf27c8adef1af8950c
ordon <ordon1313@gmail.com>
2015-10-11 12:47:36 +0200

    random list

--- mobile/src/main/java/com/mobilization/ViewAdapter.java
+++ mobile/src/main/java/com/mobilization/ViewAdapter.java
@@ -3,24 +3,35 @@ package com.mobilization;
 import android.support.annotation.DrawableRes;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public final class ViewAdapter {
 
-    private int drawableIcon;
+    private final int drawableIcon;
+    private final String text;
 
-    public ViewAdapter(@DrawableRes int drawableIcon) {
+    public ViewAdapter(@DrawableRes int drawableIcon, String text) {
         this.drawableIcon = drawableIcon;
+        this.text = text;
     }
 
     public void bind(ViewHolder holder) {
         holder.drawableView.setImageResource(drawableIcon);
+        holder.textView.setText(text);
     }
 
     public static List<ViewAdapter> longRandomList() {
         List<ViewAdapter> adapters = new ArrayList<>();
+        Random random = new Random();
+        List<Integer> drawables = Arrays.asList(R.drawable.circle1, R.drawable.circle2, R.drawable.circle3, R.drawable.circle4, R.drawable.circle5, R.drawable.circle6, R.drawable.circle7, R.drawable.circle8);
+        List<String> texts = Arrays.asList("Triangle", "Red and black", "Plus sign", "Person", "Arrow down", "Share like icon", "Arrow left", "Arrow up");
         for (int i = 0; i < 100; i++) {
-            adapters.add(new ViewAdapter(R.drawable.circle1));
+            adapters.add(new ViewAdapter(
+                            drawables.get(random.nextInt(drawables.size())),
+                            texts.get(random.nextInt(texts.size())))
+            );
         }
         return adapters;
     }
--- mobile/src/main/java/com/mobilization/ViewHolder.java
+++ mobile/src/main/java/com/mobilization/ViewHolder.java
@@ -3,13 +3,16 @@ package com.mobilization;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public final class ViewHolder extends RecyclerView.ViewHolder {
 
     public final ImageView drawableView;
+    public final TextView textView;
 
     public ViewHolder(View itemView) {
         super(itemView);
         drawableView = (ImageView) itemView.findViewById(R.id.list_item_drawable);
+        textView = (TextView) itemView.findViewById(R.id.list_item_text);
     }
 }
