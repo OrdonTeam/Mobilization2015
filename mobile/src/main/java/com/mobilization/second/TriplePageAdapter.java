e380aad05b238fabcc5494232998cdb719c7da54
ordon <ordon1313@gmail.com>
2015-10-14 23:10:22 +0200

    fragments in array

--- mobile/src/main/java/com/mobilization/second/TriplePageAdapter.java
+++ mobile/src/main/java/com/mobilization/second/TriplePageAdapter.java
@@ -6,27 +6,25 @@ import android.support.v4.app.FragmentPagerAdapter;
 
 public final class TriplePageAdapter extends FragmentPagerAdapter {
 
+    Fragment[] fragments = {new FirstPageFragment(), new SecondPageFragment(), new FirstPageFragment()};
     String[] titles = {"First", "Second", "Third"};
-
-    @Override
-    public CharSequence getPageTitle(int position) {
-        return titles[position];
-    }
 
     public TriplePageAdapter(FragmentManager fragmentManager) {
         super(fragmentManager);
     }
 
     @Override
+    public CharSequence getPageTitle(int position) {
+        return titles[position];
+    }
+
+    @Override
     public Fragment getItem(int position) {
-        if (position == 1) {
-            return new SecondPageFragment();
-        }
-        return new FirstPageFragment();
+        return fragments[position];
     }
 
     @Override
     public int getCount() {
-        return 3;
+        return fragments.length;
     }
 }
