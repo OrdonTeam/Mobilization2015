3a566fabab64c0e2b518df368cc0afc3413b6c24
ordon <ordon1313@gmail.com>
2015-10-14 23:58:17 +0200

    Remove navigation view

--- mobile/src/main/java/com/mobilization/HomeActivity.java
+++ mobile/src/main/java/com/mobilization/HomeActivity.java
@@ -1,44 +1,26 @@
 package com.mobilization;
 
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 
 import com.mobilization.dummy.SelectorInitializer;
 import com.mobilization.first.FirstFragment;
-import com.mobilization.second.SecondFragment;
 
-public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class HomeActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_activity);
-        getSupportFragmentManager().beginTransaction().replace(R.id.home_fragment_container, new FirstFragment()).commit();
-        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
-        navigationView.setNavigationItemSelectedListener(this);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        SelectorInitializer.initDummySelector(this);
+        regularInitialStuff();
     }
 
-    @Override
-    public boolean onNavigationItemSelected(MenuItem menuItem) {
-        int itemId = menuItem.getItemId();
-        if (itemId == R.id.navigation_item_1) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.home_fragment_container, new FirstFragment()).commit();
-            drawerLayout.closeDrawer(GravityCompat.START);
-            return true;
-        }
-        if (itemId == R.id.navigation_item_2) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.home_fragment_container, new SecondFragment()).commit();
-            drawerLayout.closeDrawer(GravityCompat.START);
-            return true;
-        }
-        return false;
+    private void regularInitialStuff() {
+        setContentView(R.layout.home_activity);
+        SelectorInitializer.initDummySelector(this);
+        getSupportFragmentManager().beginTransaction().replace(R.id.home_fragment_container, new FirstFragment()).commit();
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
     }
 }
