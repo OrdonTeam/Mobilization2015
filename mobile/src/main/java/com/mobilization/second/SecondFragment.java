edc64dbef8ea4e60f1b3603b79c344475c535927
ordon <ordon1313@gmail.com>
2015-10-16 23:01:59 +0200

    TabLayout

--- mobile/src/main/java/com/mobilization/second/SecondFragment.java
+++ mobile/src/main/java/com/mobilization/second/SecondFragment.java
@@ -2,6 +2,7 @@ package com.mobilization.second;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -20,9 +21,12 @@ public final class SecondFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        StandardToolbarInitializer.initToolbar(getActivity(),view);
+        StandardToolbarInitializer.initToolbar(getActivity(), view);
 
         ViewPager pager = (ViewPager) view.findViewById(R.id.view_pager);
         pager.setAdapter(new TreePagesAdapter(getFragmentManager()));
+
+        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(pager);
     }
 }
